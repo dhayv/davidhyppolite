abcdd79aec8bf24b27acdc1658e8feb8af1a674b
Dhayv <davidhyppolitedh@gmail.com>
2024-07-09 20:47:36 -0400

    addressed file not found issue and null warning

--- personalproject/src/main.tsx
+++ personalproject/src/main.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-
-const router = createBrowserRouter ([
+import {createBrowserRouter, RouterProvider, RouteObject} from 'react-router-dom'
+import { Navbar } from 'react-bootstrap'
+const routes: RouteObject[] = ([
   {
-    path: "/"
+    path: "/",
+    element: <Navbar/>
 
   }
 ])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(routes)
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
