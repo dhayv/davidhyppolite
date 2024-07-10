c382aac06bc1307bfe4b248841fd41b02f3c54c7
Dhayv <davidhyppolitedh@gmail.com>
2024-07-09 20:40:15 -0400

    added typescript and componenets folder

---
+++ personalproject/src/Components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+import { Navbar } from "react-bootstrap";
+
+
+const Menu: React.FC = () => {
+    return Menu (
+        <div>Hello, World</div>
+    )
+}
