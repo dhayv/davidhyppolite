2bf13a5f10c476e6060811275005ef00c8245b51
Dhayv <davidhyppolitedh@gmail.com>
2024-07-12 09:46:26 -0400

    Added lazy loady and suspense to rendering page

--- personalproject/src/Routes/App.tsx
+++ personalproject/src/Routes/App.tsx
@@ -1,23 +1,22 @@
-import React, { Component } from "react";
+import React, { lazy , Suspense, } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import  Menu from "../Component/Navbar"
 import Footer from "../Component/Foot";
 
-
+const Home = lazy(() => import("../Component/Home"));
 
 const App: React.FC = () => {
 
     return (
         <BrowserRouter>
-        <Menu/>
-        <Routes>
-            <Route path="/"/>
-            <Route/>
-        </Routes>
-        <Footer/>
-
-        
-        
+            <Menu/>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes >
+                    <Route path="/" element={<Home />}/>
+                </Routes>
+            </Suspense>
+            
+            <Footer/>
         </BrowserRouter>
     );
 };
