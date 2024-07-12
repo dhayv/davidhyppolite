4c6ebc00410b9581c230f49f7dde0ffd82273ada
Dhayv <davidhyppolitedh@gmail.com>
2024-07-12 09:44:45 -0400

    extended homepage/ added Intersection oberserver

--- personalproject/src/Component/Home.tsx
+++ personalproject/src/Component/Home.tsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, {lazy, Suspense, useEffect, useState, useRef } from "react";
 
+const About = lazy(() => import("../Component/About"));
+const Project = lazy(() => import ("../Component/Projects"))
 
 const Home: React.FC = () => {
+    const aboutRef = useRef<HTMLDivElement | null>(null);
+    const [isAboutVisible, setIsAboutVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (entries[0].isIntersecting) {
+                    setIsAboutVisible(true);
+                    observer.disconnect();
+                }
+            },
+            {threshold: 0.1}
+        );
+
+        if (aboutRef.current) {
+            observer.observe(aboutRef.current);
+        }
+
+        return () => observer.disconnect();
+
+    }, []);
+
+
+
+
+
     return (
-        <div>Hello World</div>
+        <section>
+            <div id="home"><h1>Hello, and welcome I am James</h1></div>
+            <p>Im a full-stack developer</p>
+
+            <div id="about" ref={aboutRef}>
+                {isAboutVisible && (
+                    <Suspense fallback={<div>Loading About..</div>}>
+                         <About/>
+                    </Suspense>
+
+                )}
+
+            </div>
+
+        </section>
     );
 };
 
