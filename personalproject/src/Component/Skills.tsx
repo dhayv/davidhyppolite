7a10b23c5f86d16a110ddbd92a41c3023df8e627
Dhayv <davidhyppolitedh@gmail.com>
2024-07-25 18:16:11 -0400

    added svg for skills

--- personalproject/src/Component/Skills.tsx
+++ personalproject/src/Component/Skills.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 const Skills: React.FC = () => {
 
     return(
         <section>
             <h1>Skills</h1>
+            <Row>
+
+                <Col>
+                </Col>
+            </Row>
+            
             <hr></hr>
         </section>
         
