a1e65f307200d2ab83723a1f8c6da78507121d6c
Dhayv <davidhyppolitedh@gmail.com>
2024-07-27 22:23:58 -0400

    recfactor code and added comments

--- personalproject/src/Component/Home.tsx
+++ personalproject/src/Component/Home.tsx
@@ -1,55 +1,54 @@
-import React, {lazy, Suspense, useEffect, useState, useRef } from "react";
+import React, { lazy, Suspense, useEffect, useState, useRef } from "react";
 import { Container } from "react-bootstrap";
 
-
+// Lazy load the components
 const About = lazy(() => import("../Component/About"));
-const Project = lazy(() => import ("../Component/Projects"))
-const Skills = lazy(() => import ("../Component/Skills"))
+const Projects = lazy(() => import("../Component/Projects"));
+const Skills = lazy(() => import("../Component/Skills"));
 
 const Home: React.FC = () => {
-    const aboutRef = useRef<HTMLDivElement | null>(null);
-    const [isAboutVisible, setIsAboutVisible] = useState(false);
+  // Reference to the about section
+  const aboutRef = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                if (entries[0].isIntersecting) {
-                    setIsAboutVisible(true);
-                    observer.disconnect();
-                }
-            },
-            {threshold: 0.1}
-        );
+  // State to track visibility of the about section
+  const [isAboutVisible, setIsAboutVisible] = useState(false);
 
-        if (aboutRef.current) {
-            observer.observe(aboutRef.current);
+  // UseEffect to observe the about section visibility
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setIsAboutVisible(true);
+          observer.disconnect();
         }
-
-        return () => observer.disconnect();
-
-    }, []);
-
-
-
-
-
-    return (
-        <Container>
-            <div id="home"><h1>Hello, and welcome I am James</h1></div>
-            <div id="about" ref={aboutRef}>
-                {isAboutVisible && (
-                    <Suspense fallback={<div>Loading About..</div>}>
-                         <About/>
-                         <Skills/>
-                         <Project/>
-                    </Suspense>
-
-                )}
-            </div>
-
-        </Container>
+      },
+      { threshold: 0.1 }
     );
-};
 
+    if (aboutRef.current) {
+      observer.observe(aboutRef.current);
+    }
+
+    // Cleanup observer on component unmount
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <Container>
+      <div id="home">
+        <h1>Hello, and welcome! I am James</h1>
+      </div>
+      <div id="about" ref={aboutRef}>
+        {isAboutVisible && (
+          <Suspense fallback={<div>Loading About...</div>}>
+            <About />
+            <Skills />
+            <Projects />
+          </Suspense>
+        )}
+      </div>
+    </Container>
+  );
+};
 
 export default Home;
