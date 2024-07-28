535670da7ff6ba093fc452aef4fdc8a072cd6575
Dhayv <davidhyppolitedh@gmail.com>
2024-07-27 22:26:54 -0400

    ligt styling

--- personalproject/src/Component/About.tsx
+++ personalproject/src/Component/About.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-
-
+import { Container, Row, Col } from "react-bootstrap";
 
 const About: React.FC = () => {
-    return (
-        <section>
-            <h2>About Me</h2>
-            <h6>I'm currently a web developer at Chingu.io</h6>
-            <hr></hr>
-        </section>
-    );
+  return (
+    <Container className="py-5">
+      <Row>
+        <Col className="text-center">
+          <h2 className="display-4 mb-4">About Me</h2>
+          <h6 className="lead mb-4">I'm currently a web developer at Chingu.io</h6>
+          <hr className="my-4" />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
-
 
 export default About;
