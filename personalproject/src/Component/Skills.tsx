d49ad2599efd35e98e5a8a52491b8ce0fc479325
Dhayv <davidhyppolitedh@gmail.com>
2024-07-27 22:21:48 -0400

    refactor

--- personalproject/src/Component/Skills.tsx
+++ personalproject/src/Component/Skills.tsx
@@ -1,26 +1,23 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
-import Icons from "../img/image"
+import Icons from "../img/image";
 
 const Skills: React.FC = () => {
+  const imageKeys = Object.keys(Icons) as Array<keyof typeof Icons>;
 
-    const imageKey = Object.keys(Icons)
-
-    console.log({imageKey})
-
-    return (
-        <section>
-          <h1>Skills</h1>
-          <Row>
-            {imageKey.map((key) => (
-              <Col key={key} xs={6} md={4} lg={2}>
-                <img className="mb-3"src={Icons[key as keyof typeof Icons]} alt={key} width={'50%'} />
-              </Col>
-            ))}
-          </Row>
-          <hr />
-        </section>
-      );
-    };
+  return (
+    <section>
+      <h1>Skills</h1>
+      <Row>
+        {imageKeys.map((key) => (
+          <Col key={key} xs={6} md={4} lg={2}>
+            <img className="mb-3 skill-icon" src={Icons[key]} alt={key} />
+          </Col>
+        ))}
+      </Row>
+      <hr />
+    </section>
+  );
+};
 
 export default Skills;
